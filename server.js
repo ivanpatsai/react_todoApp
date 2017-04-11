d323a12ade397adc99e6f160f76470fe71a7e528
Ivan Patsai <patsai.ivan@gmail.com>
2017-04-12 02:32:30 +0300

    Add PORT var to server.js

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static('public'));
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is up on port 3000!');
 });
